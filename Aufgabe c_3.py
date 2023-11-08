21d2c666c953f6cb33f437bacc201f0b7d128a70
Djrtev <150238082+Djrtev@users.noreply.github.com>
2023-11-08 17:21:20 +0100

    Add files via upload

---
+++ Aufgabe c_3.py
@@ -0,0 +1,13 @@
+zahl1 = 0
+zahl2 = 1
+zahl3 = zahl1 + zahl2
+zahl4 = 34
+
+print(0)
+print(1)
+while zahl3 <= zahl4:
+    print(zahl3)
+    zahl1 = zahl2
+    zahl2 = zahl3
+    zahl3 = zahl1 + zahl2
+    
